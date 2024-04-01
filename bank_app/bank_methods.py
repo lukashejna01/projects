e2a4d0cb93c5cbcee839550a869870c51af5e511
lukashejna01 <165669989+lukashejna01@users.noreply.github.com>
2024-04-01 22:55:47 +0200

    Update bank_methods.py

--- bank_app/bank_methods.py
+++ bank_app/bank_methods.py
@@ -3,12 +3,22 @@ import time
 import json
 
 class bankMethods:
+    """
+    Tato třída poskytuje metody pro manipulaci s bankovními účty uloženými v JSON souboru.
+
+    Atributy:
+    - acc (str): Uživatelské jméno bankovního účtu, se kterým se provádějí operace.
+    - amt (float): Aktuální zůstatek na bankovním účtu pro zadané uživatelské jméno.
+    """
     
     def __init__(self, account):
         self.acc = account
         self.amt = self.get_current_balance()
     
     def get_current_balance(self):
+        """
+        Získá aktuální zůstatek bankovního účtu z JSON souboru
+        """
         with open("users.json", "r", encoding="utf-8") as file:
             user_data = json.load(file)
             for user in user_data:
@@ -19,12 +29,18 @@ class bankMethods:
             return None
     
     def vklad(self):
+        """
+        Metoda pro vložení peněz na bankovní účet
+        """
         deposit = float(input("Zadejte částku k vložení: "))
         self.amt += deposit
         self.update_amount_in_users()
         self.print_current_balance()
     
     def vyber(self):
+        """
+        Metoda pro výběr peněz z bankovního účtu
+        """
         withdrawal = float(input("Zadejte částku k výběru: "))
         if withdrawal <= self.amt:
             self.amt -= withdrawal
@@ -34,9 +50,15 @@ class bankMethods:
             print("Nedostatečné prostředky na účtu.")
     
     def aktual(self):
+        """
+        Metoda pro zjištění aktuálního zůstatku na bankovním účtu.
+        """
         self.print_current_balance()
     
     def update_amount_in_users(self):
+        """
+        Aktualizuje zůstatek na bankovním účtu v JSON souboru.
+        """
         with open("users.json", "r+", encoding="utf-8") as file:
             user_data = json.load(file)
             for user in user_data:
@@ -48,4 +70,7 @@ class bankMethods:
             json.dump(user_data, file, indent=4, ensure_ascii=False)
     
     def print_current_balance(self):
+        """
+        Vypíše aktuální zůstatek na bankovním účtu.
+        """
         print(f"Váš aktuální zůstatek je: {self.amt} Kč.")
