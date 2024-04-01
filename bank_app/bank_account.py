f7fab66d791f5f00c447555de1177e680bdcdfd4
lukashejna01 <165669989+lukashejna01@users.noreply.github.com>
2024-04-01 22:48:34 +0200

    Update bank_account.py

--- bank_app/bank_account.py
+++ bank_app/bank_account.py
@@ -1,5 +1,12 @@
 class bankAcc:
-    
+    """
+    Tato třída slouží k uchování základních údajů uživatele bankovního účtu.
+
+    Parametry:
+    - name (str): Jméno vlastníka bankovního účtu.
+    - user_name (str): Uživatelské jméno pro přihlášení k účtu.
+    - user_pin (str): PIN kód pro přihlášení k účtu.
+    """
     def __init__(self, name, user_name, user_pin):
         self.name = name
         self.u_name = user_name
