05b3b47194a9eafc0b8862c3e85eb24d53604b9b
lukashejna01 <165669989+lukashejna01@users.noreply.github.com>
2024-05-08 22:18:04 +0200

    Delete notebook directory

--- notebook/assets/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $db_host = "localhost";
-    $db_username = "luky_note";
-    $db_password = "admin123-";
-    $db_name = "notebook";
-
-    $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
